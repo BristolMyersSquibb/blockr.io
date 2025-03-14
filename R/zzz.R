aa37087bf131dba44f93285dbb4c726c98014e69
DivadNojnarg <dgranjon@ymail.com>
2025-03-14 17:24:15 +0100

    add csv and xpt

--- R/zzz.R
+++ R/zzz.R
@@ -1,18 +1,35 @@
-register_io_blocks <- function() { # nocov start
+register_io_blocks <- function() {
+  # nocov start
   register_blocks(
     c(
       "new_readxlsx_block",
-      "new_writexlsx_block"
+      "new_writexlsx_block",
+      "new_readcsv_block",
+      "new_writecsv_block",
+      "new_readxpt_block",
+      "new_writexpt_block"
     ),
     name = c(
       "Read Excel data",
-      "Write Excel data"
+      "Write Excel data",
+      "Read csv data",
+      "Write csv data",
+      "Read xpt data",
+      "Write xpt data"
     ),
     description = c(
       "Read tabular data from an Excel file",
-      "Write tabular data to an Excel file"
+      "Write tabular data to an Excel file",
+      "Read tabular data from a csv file",
+      "Write tabular data to a csv file",
+      "Read tabular data from an xpt file",
+      "Write tabular data to an xpt file"
     ),
     category = c(
+      "data",
+      "transform",
+      "data",
+      "transform",
       "data",
       "transform"
     ),
@@ -22,7 +39,6 @@ register_io_blocks <- function() { # nocov start
 }
 
 .onLoad <- function(libname, pkgname) {
-
   register_io_blocks()
 
   invisible(NULL)
