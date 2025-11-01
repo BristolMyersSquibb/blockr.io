40a9e269b0bf71dec95d2e703d6cd350eb21a2d2
Christoph Sax <christoph.sax@gmail.com>
2025-11-01 21:27:50 +0100

    upd

--- R/zzz.R
+++ R/zzz.R
@@ -5,6 +5,17 @@ register_io_blocks <- function() {
     name = "Read data",
     description = "Read data from various file formats (CSV, Excel, SPSS, SAS, Stata, Parquet, JSON, etc.) with automatic format detection and smart adaptive UI",
     category = "data",
+    icon = "file-earmark-arrow-up",
+    package = utils::packageName(),
+    overwrite = TRUE
+  )
+
+  register_blocks(
+    "new_write_block",
+    name = "Write data",
+    description = "Write dataframes to various file formats (CSV, Excel, Parquet, Feather). Supports single or multiple inputs with download or filesystem modes",
+    category = "data",
+    icon = "floppy",
     package = utils::packageName(),
     overwrite = TRUE
   )
